525354a68249c2cb987e48c534f564300be08f7c
Darren <darrenzhang200027@gmail.com>
2020-09-25 23:25:07 -0400

    rm csome files"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-import Register from './pages/register/register.page';
 import Landing from './pages/landing/landing.js';
 import { Provider } from 'react-redux';
 import store from './redux/redux';
@@ -18,7 +17,6 @@ function App() {
     <div>
       {/* <Header /> */}
       <Route exact path='/' component={Landing} />
-      <Route exact path='/register' component={Register} />
       <Route exact path='/signup' component={Signup}/>
       <Route exact path="/home" component={Home}/>
       {/* <Footer /> */}
