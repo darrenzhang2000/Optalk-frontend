4a84fa1223f2c676cd7cc8dd29688d6a11851773
Darren <darrenzhang200027@gmail.com>
2020-10-31 18:01:47 -0400

    added form control for voice and video

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,15 @@
 import { Switch, FormControlLabel, withStyles } from '@material-ui/core'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import "./home.css"
 import Skype from "./images/skype.png"
 import GoogleMeet from "./images/google-meet.png"
 import MST from "./images/microsoft-teams.png"
-import spoken from '../../../node_modules/spoken/build/spoken.js';
 
 const IOSSwitch = withStyles((theme) => ({
     root: {
         width: 100,
         height: 50,
-        padding: 2,
+        padding: 0,
         margin: theme.spacing(1),
     },
     switchBase: {
@@ -66,67 +65,46 @@ const Home = props => {
     const [voiceChangeSelected, setVoiceChangeSelected] = useState(false)
     const [videoChangeSelected, setVideoChangeSelected] = useState(false)
 
-    const [curText , setCurText] = useState('Listening...')
-
-
-    const startCapture = () => {
-        spoken.say("Listening.....")
-        setCurText("Listening...")
-
-        spoken.listen({ continuous : true }).then( transcript =>
-            {
-                spoken.say(transcript)
-                setCurText(transcript)}
-        ).catch( e => true );
-
-    }
-
-    const continueCapture = async () => {
-        await spoken.delay(300);
-        if(spoken.recognition.continuous)
-            startCapture();
-    }
-
-    const stopCapture = () => {
-        spoken.recognition.continuous = false;
-        spoken.listen.stop();
-        setCurText("");
-    }
+    const [voiceCheck, setVoiceCheck] = useState(false);
+    const [videoCheck, setVideoCheck] = useState(false);
 
     const onSubmit = () => {
-        spoken.listen.on.end(continueCapture);
-        spoken.listen.on.error(continueCapture);
 
-        if(voiceChangeSelected){
-            startCapture();
-        }
-        else{
-            stopCapture();
-        }
     }
-
-    useEffect(() => {
-        if(voiceChangeSelected){
-            spoken.say("Voice effect turned on!");
-            setCurText("Voice effect turned on!")
-        }
-        else
-            spoken.say("Voice effect has been turned off!");
-
-            
-        
-    },[voiceChangeSelected])
-
-
 
 
     return <div className="col root">
         <div className="home">
             <p className="activeTitle">Active</p>
 
+            <div className="platforms">
+                <img src={Skype} className="img" />
+                <img src={MST} className="img" />
+                <img src={GoogleMeet} className="img" />
+            </div>
 
+            <div className="voice-change row">
+                <FormControlLabel
+                    control={<IOSSwitch checked={voiceChangeSelected} onChange={() => { setVoiceChangeSelected(!voiceChangeSelected) }} name="checkedB" />}
+                // label="iOS style"
+                />
+                <p className="text">Voice Change</p>
+            </div>
 
-            
+            <div className="video-change row">
+                <FormControlLabel
+                    control={<IOSSwitch checked={videoChangeSelected} onChange={() => { setVideoChangeSelected(!videoChangeSelected) }} name="checkedB" />}
+                // label="iOS style"
+                />
+                <p className="text">Video Change</p>
+            </div>
+
+            <button className="options">Show more options</button>
+            <div >
+                <button className="button" onClick={onSubmit}>
+                    Apply
+            </button>
+            </div>
         </div>
     </div>
 }
